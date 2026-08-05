738ab55a572075e084dbc01b742dc9c88a1995d6
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-05 22:28:19 +0530

    feat: configure Oracle JDBC database connection

--- src/main/java/com/abhilash/studentms/util/TestConnection.java
+++ src/main/java/com/abhilash/studentms/util/TestConnection.java
@@ -1,4 +1,17 @@
 package com.abhilash.studentms.util;
 
+import java.sql.Connection;
+
 public class TestConnection {
+
+    public static void main(String[] args) {
+
+        Connection connection = DBConnection.getConnection();
+
+        if (connection != null) {
+            System.out.println("Connection Successful!");
+        } else {
+            System.out.println("Connection Failed!");
+        }
+    }
 }
