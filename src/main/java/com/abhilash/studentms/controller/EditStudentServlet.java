3e94e34f1e4dee6a35a8765c9d5c55f43bc76a80
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-06 11:09:12 +0530

    feat: implement edit student servlet

--- src/main/java/com/abhilash/studentms/controller/EditStudentServlet.java
+++ src/main/java/com/abhilash/studentms/controller/EditStudentServlet.java
@@ -1,4 +1,33 @@
 package com.abhilash.studentms.controller;
 
-public class EditStudentServlet {
+import com.abhilash.studentms.dao.StudentDAO;
+import com.abhilash.studentms.dao.StudentDAOImpl;
+import com.abhilash.studentms.model.Student;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/editStudent")
+public class EditStudentServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        StudentDAO dao = new StudentDAOImpl();
+
+        Student student = dao.getStudentById(id);
+
+        request.setAttribute("student", student);
+
+        request.getRequestDispatcher("edit-student.jsp")
+                .forward(request, response);
+    }
 }
