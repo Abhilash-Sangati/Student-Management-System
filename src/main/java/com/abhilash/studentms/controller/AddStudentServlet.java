40bdbccbef7dd9bb5944c6513928af5dd1da14fe
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-06 11:08:36 +0530

    feat: implement add student servlet

--- src/main/java/com/abhilash/studentms/controller/AddStudentServlet.java
+++ src/main/java/com/abhilash/studentms/controller/AddStudentServlet.java
@@ -1,4 +1,39 @@
 package com.abhilash.studentms.controller;
 
-public class AddStudentServlet {
+import com.abhilash.studentms.dao.StudentDAO;
+import com.abhilash.studentms.dao.StudentDAOImpl;
+import com.abhilash.studentms.model.Student;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/addStudent")
+public class AddStudentServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String course = request.getParameter("course");
+        double marks = Double.parseDouble(request.getParameter("marks"));
+
+        Student student = new Student(id, name, email, course, marks);
+
+        StudentDAO dao = new StudentDAOImpl();
+
+        boolean status = dao.addStudent(student);
+
+        if (status) {
+            response.sendRedirect("index.jsp");
+        } else {
+            response.getWriter().println("Failed to Add Student");
+        }
+    }
 }
