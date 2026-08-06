ca03142412a36fafe2923b0fabe6d40b091d8b2b
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-06 11:09:17 +0530

    feat: implement update student servlet

--- src/main/java/com/abhilash/studentms/controller/UpdateStudentServlet.java
+++ src/main/java/com/abhilash/studentms/controller/UpdateStudentServlet.java
@@ -1,4 +1,36 @@
 package com.abhilash.studentms.controller;
 
-public class UpdateStudentServlet {
+import com.abhilash.studentms.dao.StudentDAO;
+import com.abhilash.studentms.dao.StudentDAOImpl;
+import com.abhilash.studentms.model.Student;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/updateStudent")
+public class UpdateStudentServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String course = request.getParameter("course");
+        double marks = Double.parseDouble(request.getParameter("marks"));
+
+        Student student = new Student(id, name, email, course, marks);
+
+        StudentDAO dao = new StudentDAOImpl();
+
+        dao.updateStudent(student);
+
+        response.sendRedirect("viewStudents");
+    }
 }
