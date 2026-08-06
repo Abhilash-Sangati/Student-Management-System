f6a6c7fc81751505f7a557765c53fb9a027e3ac7
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-06 11:08:51 +0530

    feat: implement view students servlet

--- src/main/java/com/abhilash/studentms/controller/ViewStudentsServlet.java
+++ src/main/java/com/abhilash/studentms/controller/ViewStudentsServlet.java
@@ -1,4 +1,30 @@
 package com.abhilash.studentms.controller;
 
-public class ViewStudentsServlet {
+import com.abhilash.studentms.dao.StudentDAO;
+import com.abhilash.studentms.dao.StudentDAOImpl;
+import com.abhilash.studentms.model.Student;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/viewStudents")
+public class ViewStudentsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        StudentDAO dao = new StudentDAOImpl();
+
+        List<Student> studentList = dao.getAllStudents();
+
+        request.setAttribute("studentList", studentList);
+
+        request.getRequestDispatcher("view-students.jsp").forward(request, response);
+    }
 }
