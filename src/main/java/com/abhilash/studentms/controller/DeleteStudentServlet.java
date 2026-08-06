ce22de488ab1b59763e75d1f811256e5f06b7b5c
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-06 11:08:58 +0530

    feat: implement delete student feature

--- src/main/java/com/abhilash/studentms/controller/DeleteStudentServlet.java
+++ src/main/java/com/abhilash/studentms/controller/DeleteStudentServlet.java
@@ -1,4 +1,29 @@
 package com.abhilash.studentms.controller;
 
-public class DeleteStudentServlet {
+import com.abhilash.studentms.dao.StudentDAO;
+import com.abhilash.studentms.dao.StudentDAOImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/deleteStudent")
+public class DeleteStudentServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        StudentDAO dao = new StudentDAOImpl();
+
+        dao.deleteStudent(id);
+
+        response.sendRedirect("viewStudents");
+    }
 }
