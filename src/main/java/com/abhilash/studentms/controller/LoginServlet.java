b09d85043b40b0a4210193592a4c0252f786c346
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-07 12:22:10 +0530

    Add LoginServlet for admin authentication

---
+++ src/main/java/com/abhilash/studentms/controller/LoginServlet.java
@@ -0,0 +1,39 @@
+package com.abhilash.studentms.controller;
+
+import com.abhilash.studentms.dao.AdminDAO;
+import com.abhilash.studentms.dao.AdminDAOImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                        throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        AdminDAO adminDAO = new AdminDAOImpl();
+
+        if (adminDAO.validateAdmin(username, password)) {
+
+            HttpSession session = request.getSession();
+            session.setAttribute("loggedInUser", username);
+
+            response.sendRedirect("dashboard");
+
+        } else {
+
+            response.sendRedirect("login.jsp?error=true");
+
+        }
+    }
+}
