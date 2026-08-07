58f9ec457d9e4015fe26c37b19f33fc9b77156d3
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-07 12:15:35 +0530

    Implement admin authentication using Oracle database

--- src/main/java/com/abhilash/studentms/dao/AdminDAOImpl.java
+++ src/main/java/com/abhilash/studentms/dao/AdminDAOImpl.java
@@ -1,4 +1,40 @@
 package com.abhilash.studentms.dao;
 
-public class AdminDAOImpl {
+import com.abhilash.studentms.util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class AdminDAOImpl implements AdminDAO {
+
+    @Override
+    public boolean validateAdmin(String username, String password) {
+
+        String sql = """
+                SELECT *
+                FROM ADMIN
+                WHERE USERNAME = ?
+                AND PASSWORD = ?
+                """;
+
+        try (
+                Connection connection = DBConnection.getConnection();
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(sql)
+        ) {
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            return resultSet.next();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
