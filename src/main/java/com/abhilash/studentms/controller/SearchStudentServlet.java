08b9b485f173984b4e9594ad37244ce8d650f449
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-07 12:28:40 +0530

    Add student search servlet

--- src/main/java/com/abhilash/studentms/controller/SearchStudentServlet.java
+++ src/main/java/com/abhilash/studentms/controller/SearchStudentServlet.java
@@ -1,4 +1,32 @@
 package com.abhilash.studentms.controller;
 
-public class SearchStudentServlet {
+import com.abhilash.studentms.dao.StudentDAO;
+import com.abhilash.studentms.dao.StudentDAOImpl;
+import com.abhilash.studentms.model.Student;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/searchStudent")
+public class SearchStudentServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String keyword = request.getParameter("keyword");
+
+        StudentDAO dao = new StudentDAOImpl();
+
+        List<Student> studentList = dao.searchStudents(keyword);
+
+        request.setAttribute("studentList", studentList);
+
+        request.getRequestDispatcher("view-students.jsp")
+                .forward(request, response);
+    }
 }
