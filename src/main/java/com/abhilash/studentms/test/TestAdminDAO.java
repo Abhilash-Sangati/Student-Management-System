d91a6a94d624262645165217467486d431aa869d
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-07 12:15:41 +0530

    Add AdminDAO test class

--- src/main/java/com/abhilash/studentms/test/TestAdminDAO.java
+++ src/main/java/com/abhilash/studentms/test/TestAdminDAO.java
@@ -1,4 +1,21 @@
 package com.abhilash.studentms.test;
 
+import com.abhilash.studentms.dao.AdminDAO;
+import com.abhilash.studentms.dao.AdminDAOImpl;
+
 public class TestAdminDAO {
+
+    public static void main(String[] args) {
+
+        AdminDAO dao = new AdminDAOImpl();
+
+        boolean result = dao.validateAdmin("admin", "admin123");
+
+        if (result) {
+            System.out.println("Login Successful");
+        } else {
+            System.out.println("Invalid Username or Password");
+        }
+
+    }
 }
