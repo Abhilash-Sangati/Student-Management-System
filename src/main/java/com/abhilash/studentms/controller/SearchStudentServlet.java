b0aea5d418de191b7a81cf9cbb0112d06c592ab9
Abhilash-Sangati <abhilashsangati482@gmail.com>
2026-08-10 20:12:44 +0530

    Add pagination to student search

--- src/main/java/com/abhilash/studentms/controller/SearchStudentServlet.java
+++ src/main/java/com/abhilash/studentms/controller/SearchStudentServlet.java
@@ -2,12 +2,9 @@ package com.abhilash.studentms.controller;
 
 import com.abhilash.studentms.dao.StudentDAO;
 import com.abhilash.studentms.dao.StudentDAOImpl;
-import com.abhilash.studentms.model.Student;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -26,11 +23,39 @@ public class SearchStudentServlet extends HttpServlet {
             keyword = "";
         }
 
+        int page = 1;
+        int pageSize = 5;
+
+        String pageParameter = request.getParameter("page");
+
+        if (pageParameter != null) {
+            try {
+                page = Integer.parseInt(pageParameter);
+
+                if (page < 1) {
+                    page = 1;
+                }
+
+            } catch (NumberFormatException e) {
+                page = 1;
+            }
+        }
+
         StudentDAO dao = new StudentDAOImpl();
-        List<Student> students = dao.searchStudents(keyword);
+
+        List<com.abhilash.studentms.model.Student> students =
+                dao.searchStudentsByPage(keyword, page, pageSize);
+
+        int totalStudents =
+                dao.getSearchStudentCount(keyword);
+
+        int totalPages =
+                (int) Math.ceil((double) totalStudents / pageSize);
 
         request.setAttribute("studentList", students);
         request.setAttribute("keyword", keyword);
+        request.setAttribute("currentPage", page);
+        request.setAttribute("totalPages", totalPages);
 
         request.getRequestDispatcher("view-students.jsp")
                 .forward(request, response);
